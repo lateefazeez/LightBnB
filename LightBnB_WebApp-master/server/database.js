c5c9e6d7a59f0e80dfea41f68fb0922e67341362
Lateef Azeez <codelate18@gmail.com>
2021-10-14 18:04:12 -0600

    Implement add property

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -128,11 +128,9 @@ const getAllProperties = (options, limit = 10) => {
 
   let searchResult = searchProperties(queryString, queryParams, options, limit);
   
-  console.log(searchResult.qString, searchResult.qParams);
   return pool
     .query(searchResult.qString, searchResult.qParams)
     .then((result) => {
-      console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
@@ -148,10 +146,32 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const owner_id = property.owner_id;
+  const title = property.title;
+  const description = property.description;
+  const thumbnail_photo_url = property.thumbnail_photo_url;
+  const cover_photo_url = property.cover_photo_url;
+  const cost_per_night = property.cost_per_night;
+  const street = property.street;
+  const city = property.city;
+  const province = property.province;
+  const post_code = property.post_code;
+  const country = property.country;
+  const parking_spaces = property.parking_spaces;
+  const number_of_bathrooms = property.number_of_bathrooms;
+  const number_of_bedrooms = property.number_of_bedrooms;
+
+  return pool
+    .query(`
+    INSERT INTO properties (
+      title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, province, city, country, street, post_code) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+  `, [title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, province, city, country, street, post_code])
+    .then(property => {
+      return property.rows;
+    })
+    .catch(err => console.log(err.message));
 };
 
 const searchProperties = (queryString, queryParams, options, limit) => {
